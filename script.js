bbd2f9b9ae6382a6613e0b26a805049b0bfd367f
EmmaSmedley87 <EmmaLondon1987@hotmail.com>
2022-11-15 20:15:26 +0000

    created sin wave for Enemy2 in js

--- script.js
+++ script.js
@@ -1,9 +1,75 @@
+// ENEMY 1 MOVEMENT //
+// /** @type {HTMLCanvasElement} */
+// const canvas = document.getElementById("canvas1");
+// const ctx = canvas.getContext("2d");
+// CANVAS_WIDTH = canvas.width = 500;
+// CANVAS_HEIGHT = canvas.height = 1000;
+// const numberOfEnemies = 10;
+// const enemiesArray = [];
+
+// let gameFrame = 0;
+
+// class Enemy {
+//   constructor() {
+//     this.image = new Image();
+//     this.image.src = "images/enemy1.png";
+//     // this.speed = Math.random() * 4 - 2;
+//     this.spriteWidth = 293;
+//     this.spriteHeight = 155;
+//     this.width = this.spriteWidth / 2.5;
+//     this.height = this.spriteHeight / 2.5;
+//     this.x = Math.random() * (canvas.width - this.width);
+//     this.y = Math.random() * (canvas.height - this.height);
+//     this.frame = 0;
+//     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+//   }
+//   update() {
+//     this.x += Math.random() * 15 - 7.5;
+//     this.y += Math.random() * 10 - 2.5;
+//     // animate sprites
+//     if (gameFrame % this.flapSpeed === 0) {
+//       this.frame > 4 ? (this.frame = 0) : this.frame++;
+//     }
+//   }
+//   draw() {
+//     ctx.drawImage(
+//       this.image,
+//       this.frame * this.spriteWidth,
+//       0,
+//       this.spriteWidth,
+//       this.spriteHeight,
+//       this.x,
+//       this.y,
+//       this.width,
+//       this.height
+//     );
+//   }
+// }
+
+// for (let i = 0; i < numberOfEnemies; i++) {
+//   enemiesArray.push(new Enemy());
+// }
+
+// function animate() {
+//   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+//   enemiesArray.forEach((enemy) => {
+//     enemy.update();
+//     enemy.draw();
+//   });
+//   gameFrame++;
+//   // ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+//   requestAnimationFrame(animate);
+// }
+// animate();
+// ENEMY 1 MOVEMENT ENDS//
+
+// ENEMY 2 MOVEMENT //
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 300;
+const numberOfEnemies = 50;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -11,20 +77,24 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = "images/enemy1.png";
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    // this.speed = Math.random() * 4 - 2;
-    this.spriteWidth = 293;
-    this.spriteHeight = 155;
+    this.image.src = "images/enemy2.png";
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = Math.random() * 2;
   }
   update() {
-    this.x += Math.random() * 5 - 2.5;
-    this.y += Math.random() * 5 - 2.5;
+    this.x -= this.speed;
+    // wave animation
+    this.y += Math.sin(this.angle);
+    this.angle += 0.05;
+    if (this.x + this.width < 0) this.x = canvas.width;
     // animate sprites
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
@@ -60,3 +130,4 @@ function animate() {
   requestAnimationFrame(animate);
 }
 animate();
+// ENEMY 2 MOVEMENT ENDS //
